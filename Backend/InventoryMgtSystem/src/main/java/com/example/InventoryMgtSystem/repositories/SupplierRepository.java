0a4655c1202a80c22184f05a52becdcc5e523e5b
Kasun Rajapaksha <kasunrajapaksha2001@gmail.com>
2025-06-23 20:56:14 +0530

    interfaces created (repositories)

---
+++ Backend/InventoryMgtSystem/src/main/java/com/example/InventoryMgtSystem/repositories/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.example.InventoryMgtSystem.repositories;
+
+
+import com.example.InventoryMgtSystem.models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+}
+
